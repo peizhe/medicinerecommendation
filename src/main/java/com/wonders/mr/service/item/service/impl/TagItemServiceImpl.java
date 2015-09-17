f261f14065b1a0fc882a3ae939d53dc94bb2453b
xiangbin <xiangbin@wondersgroup.com>
2015-09-17 14:37:45 +0800

    提交搜索框功能代码

--- src/main/java/com/wonders/mr/service/item/service/impl/TagItemServiceImpl.java
+++ src/main/java/com/wonders/mr/service/item/service/impl/TagItemServiceImpl.java
@@ -1,11 +1,13 @@
 package com.wonders.mr.service.item.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wonders.bud.framework.common.util.QueryParam;
 import com.wonders.mr.service.item.dao.ItemDao;
 import com.wonders.mr.service.item.dao.TagItemDao;
 import com.wonders.mr.service.item.modal.po.ItemPO;
@@ -22,8 +24,15 @@ public class TagItemServiceImpl implements TagItemService {
 
 	@Override
 	public List<ItemPO> findById(long id) {
-		TagItemPO tagItem = tagItemDao.get(id);
-		String itemIdList = tagItem.getItemIdList();
+		
+		QueryParam param = new QueryParam();
+		
+		Map<String, Object> eq = new HashMap<>();
+		eq.put("tagId", id);
+		param.setEq(eq);
+		TagItemPO tagItemPO = tagItemDao.findByAnd(param).get(0);
+		
+		String itemIdList = tagItemPO.getItemIdList();
 		String[] ids = itemIdList.split(",");
 		List<ItemPO> itemList = new ArrayList<ItemPO>();
 		for (String item : ids) {
